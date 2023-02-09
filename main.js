2bc1a3f1e0f1a56ea861bc4aa87c1f358521585a
Dirk Voss <mail@dirk-voss.com>
2023-02-09 16:39:11 +0100

    1st connect working and added user and password

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 'use strict';
 
-/*
- * Created with @iobroker/create-adapter v2.3.0
- */
-
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-const axios = require('axios').default;
-
-// Load your modules here, e.g.:
-// const fs = require("fs");
 
 class Oscam extends utils.Adapter {
 
@@ -33,61 +23,34 @@ class Oscam extends utils.Adapter {
      * Is called when databases are connected and adapter received configuration.
      */
     async onReady() {
-        // Initialize your adapter here
 
-        // Reset the connection indicator during startup
-        this.setState('info.connection', false, true);
+        const oscamIpAdress = 'http://' + this.config.serverIp ;
+        const oscamUser = this.config.userName;
+        const oscamPassword = this.config.password;
+        const oscamUrl = '/oscamapi.html?part=status';
 
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
-
-        /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-        */
-        await this.setObjectNotExistsAsync('testVariable', {
-            type: 'state',
-            common: {
-                name: 'testVariable',
-                type: 'boolean',
-                role: 'indicator',
-                read: true,
-                write: true,
-            },
-            native: {},
-        });
-
-        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
-        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-        // this.subscribeStates('lights.*');
-        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-        // this.subscribeStates('*');
-
-        /*
-            setState examples
-            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-        */
-        // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
-
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
-
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
-
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
+        const digestRequest = require('request-digest')(oscamUser, oscamPassword);
+        digestRequest.requestAsync({
+            host: oscamIpAdress,
+            path: oscamUrl,
+            port: 80,
+            method: 'GET',
+            excludePort: false,
+            headers: {
+                'Custom-Header': 'OneValue',
+                'Other-Custom-Header': 'OtherValue'
+            }
+        })
+            .then(function (response) {
+                console.log(response.body);
+            })
+            .catch(function (error) {
+                console.log(error.statusCode);
+                console.log(error.body);
+            });
     }
+
+
 
     /**
      * Is called when adapter shuts down - callback has to be called under any circumstances!
